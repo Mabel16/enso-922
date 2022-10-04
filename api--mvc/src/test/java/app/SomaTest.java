d9c7151fdc79a439c59a9e34b3877d2947ac2c1d
Mabel16 <madg1@aluno.ifal.edu.br>
2022-10-04 20:24:50 +0000

    soma_test

---
+++ api--mvc/src/test/java/app/SomaTest.java
@@ -0,0 +1,27 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SomaTest {
+
+    @Test
+    public void verificarCasoBase(){
+
+        Soma soma = new Soma();
+        double resultado = soma.calcula("4","9");
+        assertEquals(13.0, resultado);
+
+    }
+
+    @Test
+    public void verificarSomaDouble(){
+
+        Soma soma = new Soma();
+        double resultado = soma.calcula("7.5", "3");
+        assertEquals(10.5, resultado);
+
+    }
+    
+}
